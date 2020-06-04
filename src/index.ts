dd9d0ebe715a3d7ee5cdee4d70fb4c106b4b2a00
Kuba <s17838@pjwstk.edu.pl>
2020-06-05 01:12:28 +0200

    Init firebase and firestore, simple get with all movies

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions';
+import * as serviceAccount from '../ServiceAccountKey.json';
 
 const server = express();
 
 export const createNestServer = async (expressInstance) => {
+
+  admin.initializeApp({
+    // @ts-ignore
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://bejflix.firebaseio.com'
+  });
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
