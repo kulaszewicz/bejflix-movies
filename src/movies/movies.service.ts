fde45c7dd243447645c482660cb25da014069bb3
Kuba <s17838@pjwstk.edu.pl>
2020-06-05 11:23:22 +0200

    Added addMovie and getByGenre endpoints

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin'
+import { Movie } from "./interfaces/movie.interface";
 
 @Injectable()
 export class MoviesService {
@@ -8,5 +9,18 @@ export class MoviesService {
   async findAll():  Promise<FirebaseFirestore.DocumentData[]> {
     const snapshot = await this.db.collection('movies').get()
     return snapshot.docs.map(movie => movie.data());
+  }
+
+  async getByGenre(genre: string):  Promise<FirebaseFirestore.DocumentData[]> {
+    const snapshot = await this.db.collection('movies').where('genre', '==', genre).get()
+    return snapshot.docs.map(movie => movie.data());
+  }
+
+  async addMovie(Movie: Movie): Promise<FirebaseFirestore.DocumentData[]> {
+    return await this.db.collection('movies').doc().set(Movie).then(() => {
+      return 'Movie added successfully'
+    }).catch((err) => {
+      return err;
+    });
   }
 }
